49ef94d6ec7daee2dc905ac5e7e5ae67e0de5d5a
DeuterGraves <puckles@gmail.com>
2018-10-08 15:38:16 +0100

    update RESTful path now working, save updated

--- many_to_many_gym_sinatra/controllers/members_controller.rb
+++ many_to_many_gym_sinatra/controllers/members_controller.rb
@@ -18,7 +18,7 @@ end
 post "/members/:id" do
   @member = Member.new(params)
   @member.save()
-  redirect to("/members")
+  redirect to("/members/#{@member.id}")
 end
 
 # SHOW
@@ -27,4 +27,17 @@ get "/members/:id" do
   @sessions = @member.sessions()
   erb(:"members/show")
 end
-# needs: update, delete, book a class for a member
+# needs: delete, book a class for a member
+
+# EDIT
+get "/members/:id/edit" do
+  @member = Member.find(params[:id])
+  erb(:"members/edit")
+end
+
+# UPDATE
+post "/members/:id/edit" do
+  @member = Member.new(params)
+  @member.update()
+  redirect to("/members/#{@member.id}")
+end
